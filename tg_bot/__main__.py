11db253db00afb237d05d2a96af58a4dd846be47
deepanshu143 <79148176+deepanshu143@users.noreply.github.com>
2021-04-13 18:33:02 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -49,7 +49,7 @@ Hello! my name *{}*.
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-TECHNO_IMG = "https://telegra.ph/file/7e761f864d2ef42fe9446.jpg"
+TECHNO_IMG = "https://telegra.ph/file/e34efb7be784b7d590cc5.jpg"
 IMPORTED = {}
 MIGRATEABLE = []
 HELPABLE = {}
@@ -142,15 +142,15 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/darkiecrif")],
-                                                                                   [InlineKeyboardButton(text="ADD LegendPikacu TO YOUR GROUP",
+                                                                       url="t.me/MTY_IZ")],
+                                                                                   [InlineKeyboardButton(text="ADD Lucario TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
-                                                                       url="https://github.com/legendx22/GRANDROBOT")
+                                                                       url="https://github.com/deepanshu143/Lucario")
                                                                                  ]]))
 
     else:
-        update.effective_message.reply_text("LegendPikachuYT's, Soldier On duty ")
+        update.effective_message.reply_text("lucario is On duty ")
 
 
 def send_start(bot, update):
